f5b8be38b96cc88ae77f84de0543143ad89eb998
Rakesh Gupta <grakesh18@gmail.com>
2016-04-20 15:52:10 +0530

    committed latest changes

--- test/unit/mongo-transform-spec.js
+++ test/unit/mongo-transform-spec.js
@@ -1,33 +1,54 @@
-var chai = require('chai'),
-	expect = chai.expect,
-    sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
 
-chai.use(require('sinon-chai'));
+describe('Test mongo transformation', function(){
 
-chai.use(require('chai-fs'));
+    before(function(){
+        config 				= require('../../scripts/JadeTransform/config-mongo.js');
+        transform 			= require('../../scripts/JadeTransform/util/mongo-transform.js');
+        prompts				= require('../../scripts/JadeTransform/config-prompt.js');
+        transformProperties = transform.populateTransformObjects(config);
+        chai.use(require('chai-fs'));
+        path = require('path');
+    });
 
-describe('mongo-transform', function() {
-	var transformProperties;
-	var env;
-	var config;
-	var transform;
+    describe('Mongo transformation test input folder', function(){
 
-	beforeEach(function () {
-		config 				= require('../../scripts/JadeTransform/config-mongo.js');
-		prompts				= require('../../scripts/JadeTransform/config-prompt.js');
-		transform 			= require('../../scripts/JadeTransform/util/transform.js');
-		transformProperties = transform.populateTransformObjects(config)
-	})
+        it('should return output_to_folder_only value that matches transformProperty', function () {
+            expect(transformProperties.output_to_folder_only).equal('n');
+        });
 
-	describe('populateTransformObjects - New customer unit test', function () {
-		it('should return output_to_folder_only value that matches transformProperty', function () {
-			expect(transformProperties.output_to_folder_only).equal('n');
-		});
-		it('should return input_folder value that matches transformProperty', function () {
-			expect(transformProperties.input_folder).equal('mongo_english');
-		});
-		it('should return output_folder value that matches transformProperty', function () {
-			expect(transformProperties.output_folder).equal('mongo_english_out');
-		});
-	});
+        it('should return input_folder value that matches transformProperty', function () {
+            expect(transformProperties.input_folder).equal('mongo_english');
+        });
+
+        it('should return output_folder value that matches transformProperty', function () {
+            expect(transformProperties.output_folder).equal('mongo_english_out');
+        });
+
+        it('should match input folder prompt text (Enter a relative path to the Mongo JSON input folder containing the .json files to be modified.)', function(){
+            expect(prompts.input_folder.trim()).equal('Enter a relative path to the Mongo JSON input folder containing the .json files to be modified.'.trim());
+        });
+
+        it('should be valid and non empty directory', function(){
+            expect(path.resolve('../scripts/JadeTransform/'+ config.input_folder)).to.be.a.directory().and.not.empty;
+
+            expect(path.resolve('../scripts/JadeTransform/'+ config.input_folder +'/users.json')).to.be.a.file().with.json;
+            
+            expect(path.resolve('../scripts/JadeTransform/'+ config.input_folder +'/templateDefinitions.json')).to.be.a.file().with.json;
+
+            //expect(path.resolve('../scripts/JadeTransform/'+ config.input_folder +'/productDefinitions.json')).to.be.a.file().with.json;
+
+            expect(path.resolve('../scripts/JadeTransform/'+ config.input_folder +'/notificationDefinitions.json')).to.be.a.file().with.json;
+
+            //expect(path.resolve('../scripts/JadeTransform/'+config.input_folder+'/companies-v1.3.json')).to.be.a.file().with.json;
+            
+        });
+    });
+
+    describe('Mongo transformation test output folder', function(){
+        it('should match output folder prompt text (Enter a relative path to the Mongo JSON output folder where the transformed .json files will be written to.)', function(){
+            expect(prompts.output_folder.trim()).equal('Enter a relative path to the Mongo JSON output folder where the transformed .json files will be written to.'.trim());
+        });
+    });
 });
